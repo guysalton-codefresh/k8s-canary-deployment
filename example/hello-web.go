ae18b41bdbee5d41abb8f3e9e6b9442622246c07
Guy Salton <47520615+guysalton-codefresh@users.noreply.github.com>
2019-08-06 14:27:02 +0300

    Update hello-web.go

--- example/hello-web.go
+++ example/hello-web.go
@@ -6,7 +6,7 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "I am a GO application version 2 running in a Docker image - this is the new version 2")
+	fmt.Fprintf(w, "I am a GO application version 2 running in a Docker image - this is the new version 3")
 
 }
 
